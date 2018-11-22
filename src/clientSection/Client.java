0e4f7d4e967b26349828c5c2766fa5d2bb7745b4
1670005341 <43111075+1670005341@users.noreply.github.com>
2018-11-22 15:47:12 +0700

    plus

--- src/clientSection/Client.java
+++ src/clientSection/Client.java
@@ -1,32 +1,48 @@
-package GUI;
+package clientSection;
 
-public class MainMenu {
+public class Client {
 	private String clientName;
 	private int clientPhoneNumber;
 	private int clientProductOrdered;
 	private String clientAddress;
+	private String deliveryCarrier;
 	
 	public String getClientName() {
 		return clientName;
 	}
+	
 	public void setClientName(String clientName) {
 		this.clientName = clientName;
 	}
+	
 	public String getClientAddress() {
 		return clientAddress;
 	}
+	
 	public void setClientAddress(String clientAddress) {
 		this.clientAddress = clientAddress;
 	}
+	
+	public String getDeliveryCarrier() {
+		return deliveryCarrier;
+	}
+
+	public void setDeliveryCarrier(String deliveryCarrier) {
+		this.deliveryCarrier = deliveryCarrier;
+	}
+
 	public int getClientPhoneNumber() {
 		return clientPhoneNumber;
 	}
+	
 	public void setClientPhoneNumber(int clientPhoneNumber) {
 		this.clientPhoneNumber = clientPhoneNumber;
 	}
+	
 	public int getClientProductOrdered() {
 		return clientProductOrdered;
 	}
+	
 	public void setClientProductOrdered(int clientProductOrdered) {
 		this.clientProductOrdered = clientProductOrdered;
 	}	
---
+++ src/inventorySection/Inventory.java
@@ -0,0 +1,21 @@
+package inventorySection;
+
+public class Inventory {
+	private int stockNumber;
+	private String itemName;
+	
+	public int getStockNumber() {
+		return stockNumber;
+	}
+	public void setStockNumber(int stockNumber) {
+		this.stockNumber = stockNumber;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	
+	
+}
