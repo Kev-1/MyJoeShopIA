30ae433ae13edf41ef313c73425782f7c0798eea
1670005341 <43111075+1670005341@users.noreply.github.com>
2018-12-05 13:16:39 +0700

    up

--- src/InitialScreen.java
+++ src/InitialScreen.java
@@ -10,32 +10,31 @@ public class InitialScreen {
 	
 	public InitialScreen() {
 		storageOption = new String();
-		byte[] data = RAF.readFromFile(RAF.FILE_PATH, 0, 100);
+		byte[] data = raf.readFromFile(InitialScreen.FILE_PATH, 0, 100);
 		String stringData = data.toString();
 		if(stringData.equals("cloud")) {
-			
+			storageOption = "cloud";
+			new SummaryMenu();
+			return;
 		} else if(stringData.equals("local")) {
 			storageOption = "local";
 			new SummaryMenu();
+			return;
 		} else {
 			mainFrame = new JOptionPane();
 			int input = JOptionPane.showOptionDialog(null, "Do you want to use Cloud Storage to store your files?", "Status", JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE, null, null, null);
-			
-			switch(input) 
-			{
-			case JOptionPane.YES_OPTION:
+			if(input == JOptionPane.YES_OPTION) {
 				System.out.println("User selected cloud!");
-				RAF.writeToFile(RAF.FILE_PATH, "database", 0);
+				raf.writeToFile(InitialScreen.FILE_PATH, "database", 0);
 				storageOption = "database";
 				new SummaryMenu();
-			case JOptionPane.NO_OPTION:
+			}
+			if(input == JOptionPane.NO_OPTION) {
 				System.out.println("User selected local!");
-				RAF.writeToFile(RAF.FILE_PATH, "local", 0);
+				raf.writeToFile(InitialScreen.FILE_PATH, "local", 0);
 				storageOption = "local";
 				new SummaryMenu();
 			}
 		}
-		
 	}
-	
 }
